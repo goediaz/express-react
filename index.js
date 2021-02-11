60c0074701da0c913355249a60646c705400dfe6
rodrigo diaz <diro@erni-espana.es>
2021-02-11 16:34:48 +0100

    Removing body parser

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
 const apiRouter = require('./routes/api.js');
 const port = process.env.PORT || 5000;
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
 // Routes
 app.use('/api', apiRouter);
